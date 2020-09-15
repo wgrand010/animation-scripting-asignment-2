76226f338a10174d02186137cf77241403476b69
wgrand <wgrand010@gmail.com>
2020-09-15 18:23:38 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+class SceneFile(object):
+    def __init__(self, folder_path, descriptor, task, ver, ext):
+        self.folder_path = folder_path
+        self.descriptor = descriptor
+        self.task = task
+        self.ver = ver
+        self.ext = ext
+
+    
